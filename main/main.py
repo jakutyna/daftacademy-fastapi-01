eab0ffbf1efc077525e553dfe02e1c9f979e4079
Jacek <jakutyna@gmail.com>
2021-04-19 23:54:48 +0200

    add exercise 3

--- main/main.py
+++ main/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Request
+from hashlib import sha512
+
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
@@ -18,3 +20,14 @@ def method_view(request: Request):
 @app.post('/method', status_code=201)
 def method_post_view():
     return {"method": "POST"}
+
+
+# Ex3
+@app.get('/auth')
+def auth_view(response: Response, password: str = '', password_hash: str = ''):
+    password_sha512 = sha512(password.encode('utf-8')).hexdigest()
+    if password_sha512 == password_hash:
+        response.status_code = 204
+    else:
+        response.status_code = 401
+    return response.status_code
