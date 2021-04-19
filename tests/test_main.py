5484fe930c6b645e0d7416d66342bb1b669fcef5
Jacek <jakutyna@gmail.com>
2021-04-19 22:24:01 +0200

    initial commit

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+import pytest
+from fastapi.testclient import TestClient
+from main.main import app
+
+client = TestClient(app)
+
+
+def test_hello_world():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello world!"}
