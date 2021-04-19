a7e2b0e5b08a114db7056d79683e665f8aa16cdf
Jacek <jakutyna@gmail.com>
2021-04-19 23:05:12 +0200

    add exercise 1.2

--- main/main.py
+++ main/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
+# Ex1
 @app.get('/')
 def index_view():
     return {"message": "Hello world!"}
+
+
+# Ex2
+@app.api_route('/method', methods=["GET", "DELETE", "PUT", "OPTIONS"])
+def method_view(request: Request):
+    return {"method": request.method}
+
+
+@app.post('/method', status_code=201)
+def method_post_view():
+    return {"method": "POST"}
--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,26 @@ def test_hello_world():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world!"}
+
+
+def test_hello_world_online():
+    response = client.get('https://da-plu-2021-jakutyna.herokuapp.com/')
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello world!"}
+
+def test_method():
+    response = client.get('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+    response = client.put('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "PUT"}
+    response = client.options('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "OPTIONS"}
+    response = client.delete('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "DELETE"}
+    response = client.post('/method')
+    assert response.status_code == 201
+    assert response.json() == {"method": "POST"}
